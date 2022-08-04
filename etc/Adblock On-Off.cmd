11116590193f4ef07a76c44038a7a05081483953
Iosif Mihaescu <63189702+IosifMihaescu@users.noreply.github.com>
2022-08-04 19:25:03 +0200

    Update Adblock On-Off.cmd
    Added a "shortcut changer"

--- etc/Adblock On-Off.cmd
+++ etc/Adblock On-Off.cmd
@@ -1,6 +1,7 @@
 @echo off
 cd C:\Windows\System32\drivers\etc\
 IF NOT EXIST "C:\Windows\System32\drivers\etc\adblock-enabled" (
+ren "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Adblock OFF.lnk" "Adblock ON.lnk"
 ren "C:\Windows\System32\drivers\etc\hosts" hosts-default
 ren "C:\Windows\System32\drivers\etc\hosts-adblock" hosts
 type nul > adblock-enabled
@@ -11,6 +12,7 @@ echo Adblock Enabled
 choice /T 3 /C X /D X /N
 exit
 ) ELSE (
+ren "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Adblock ON.lnk" "Adblock OFF.lnk"
 ren "C:\Windows\System32\drivers\etc\hosts" hosts-adblock
 ren "C:\Windows\System32\drivers\etc\hosts-default" hosts
 del "C:\Windows\System32\drivers\etc\adblock-enabled"
