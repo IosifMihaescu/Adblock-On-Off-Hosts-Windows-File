9c0146f8c5e66aae352a7ebcf09299699d7db22e
Iosif Mihaescu <63189702+IosifMihaescu@users.noreply.github.com>
2022-08-04 19:23:01 +0200

    Update SETUP (Run as Administrator).cmd
    Updated installation process

--- SETUP (Run as Administrator).cmd
+++ SETUP (Run as Administrator).cmd
@@ -1,9 +1,34 @@
 @echo off
-copy "C:\Windows\System32\drivers\etc\hosts" "C:\Windows\System32\drivers\etc\hosts-adblock"
+
+SET CurrentDir=%~dp0
+copy /y "%CurrentDir%etc\Adblock On-Off.cmd" "C:\Windows\System32\drivers\etc\Adblock On-Off.cmd"
+IF EXIST "C:\Windows\System32\drivers\etc\adblock-enabled" (
+ren "C:\Windows\System32\drivers\etc\hosts" hosts-adblock
+del "C:\Windows\System32\drivers\etc\hosts-adblock"
+ren "C:\Windows\System32\drivers\etc\hosts-default" hosts
+del "C:\Windows\System32\drivers\etc\adblock-enabled"
+) ELSE (
+cls
+)
+copy /y "C:\Windows\System32\drivers\etc\hosts" "C:\Windows\System32\drivers\etc\hosts-adblock"
 SET CurrentDir=%~dp0
 type "%CurrentDir%etc\hosts-adblock.example" >> "C:\Windows\System32\drivers\etc\hosts-adblock"
-copy "%CurrentDir%etc\Adblock On-Off.cmd" "C:\Windows\System32\drivers\etc\Adblock On-Off.cmd"
-copy "%CurrentDir%etc\Adblock On-Off.lnk" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Adblock On-Off.lnk" 
+IF EXIST "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Adblock ON.lnk" (
+del "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Adblock ON.lnk"
+) ELSE (
+cls
+)
+IF EXIST "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Adblock OFF.lnk" (
+del "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Adblock OFF.lnk"
+) ELSE (
+cls
+)
+IF EXIST "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Adblock On-Off.lnk" (
+del "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Adblock On-Off.lnk"
+copy /y "%CurrentDir%etc\Adblock OFF.lnk" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Adblock OFF.lnk" 
+) ELSE (
+copy /y "%CurrentDir%etc\Adblock OFF.lnk" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Adblock OFF.lnk" 
+)
 cls
 IF NOT EXIST "C:\Windows\System32\drivers\etc\hosts-adblock" (
 color 0C
